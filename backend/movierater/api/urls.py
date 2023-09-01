78c720265ccee952258602c17fa5c6dc330147b2
sakib387 <jucse28.387@gmail.com>
2023-09-02 00:17:45 +0600

    added view by serializer

--- backend/movierater/api/urls.py
+++ backend/movierater/api/urls.py
@@ -1,8 +1,12 @@
 from django.contrib import admin
-from django.urls import path,include
+from django.urls import path, include
 from rest_framework import routers
+from .views import MovieViewset, RatingViewset
 
-router=routers.DefaultRouter()
+router = routers.DefaultRouter()
+router.register('/movie', MovieViewset)
+router.register('/rating', RatingViewset)
+
 urlpatterns = [
     path('', include(router.urls)),
 ]
--- backend/movierater/api/views.py
+++ backend/movierater/api/views.py
@@ -1,3 +1,13 @@
 from django.shortcuts import render
-
+from rest_framework import viewsets
+from . models import Movie,Rating
+from .serializers import MovieSerializer,RatingSerializer
 # Create your views here.
+
+class MovieViewset(viewsets.ModelViewSet):
+    queryset=Movie.objects.all()
+    serializer_class=MovieSerializer
+
+class RatingViewset(viewsets.ModelViewSet):
+    queryset=Rating.objects.all()
+    serializer_class=RatingSerializer
