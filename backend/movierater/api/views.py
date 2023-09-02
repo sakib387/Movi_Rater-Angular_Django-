1ee119c0b86e738ca638cfe7c947a2f38ff1cde7
sakib387 <jucse28.387@gmail.com>
2023-09-02 21:02:10 +0600

    create & update

--- backend/movierater/api/views.py
+++ backend/movierater/api/views.py
@@ -1,6 +1,7 @@
 from django.shortcuts import render
 from rest_framework.response import Response
 from rest_framework.decorators import action
+from django.contrib.auth.models import User
 from rest_framework import viewsets,status
 from . models import Movie,Rating
 from .serializers import MovieSerializer,RatingSerializer
@@ -11,11 +12,25 @@ class MovieViewset(viewsets.ModelViewSet):
     serializer_class=MovieSerializer
     @action(detail=True,methods=['POST'])
     def rate_movie(self,request,pk=None):
+        print(request.data)
         if 'stars' in request.data:
             movie=Movie.objects.get(id=pk)
-            print(movie.title)
-            response={'message':'its working now'}
-            return Response(response,status=status.HTTP_200_OK)
+            #user=request.user
+            user=User.objects.get(id=1)
+            stars=request.data['stars']
+            try:
+                rating=Rating.objects.get(user=user.id,movie=movie.id)
+                rating.stars=stars
+                rating.save()
+                serializer=RatingSerializer(rating,many=False)
+                response={'message':'Rating updated','result':serializer.data}
+                return Response(response,status=status.HTTP_200_OK)
+                
+            except:
+                rating= Rating.objects.create(user=user,movie=movie,stars=stars)
+                serializer=RatingSerializer(rating,many=False)
+                response={'message':'Rating created','result':serializer.data}
+                return Response(response,status=status.HTTP_200_OK)
         else:
             response={'message':'No stars found'}
             return Response(response,status=status.HTTP_400_BAD_REQUEST)
